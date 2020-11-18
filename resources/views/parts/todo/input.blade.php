1ecb7da2a75ca99438bf713ecaf309c609c1faae
Nauraj2020 <naurajsharma19s016@gmail.com>
2020-11-18 11:30:43 +0900

    へんこう

--- resources/views/parts/todo/input.blade.php
+++ resources/views/parts/todo/input.blade.php
@@ -6,4 +6,4 @@
     <label for="due_date">期限</label>
     <input type="date" class="form-control" name="due_date" placeholder="2020/10/31" required>
 </div>
-<input type="hidden" name="_token" value="{{ csrf_token() }}"> 
+<input type="hidden" name="_token" value="{{ csrf_token() }}">
